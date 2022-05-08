e5013fa2593a6600a3b55f2ac6916ab2d543409f
Bofeng Pan <nog3952149@outlook.com>
2022-05-09 00:00:30 +0800

    更改兵卒ID

--- ProjectChineseChess/GameManager.cpp
+++ ProjectChineseChess/GameManager.cpp
@@ -19,7 +19,7 @@ namespace ProjectChineseChess
 		Advisor^ advisorB1 = gcnew Advisor(Player::BLACK, 1), ^ advisorB2 = gcnew Advisor(Player::BLACK, 2);
 		Cannon^ cannonB1 = gcnew Cannon(Player::BLACK, 1), ^ cannonB2 = gcnew Cannon(Player::BLACK, 2);
 		Soldier^ soldierB1 = gcnew Soldier(Player::BLACK, 1), ^ soldierB2 = gcnew Soldier(Player::BLACK, 2), ^ soldierB3 = gcnew Soldier(Player::BLACK, 3);
-		Soldier^ soldierB4 = gcnew Soldier(Player::BLACK, 1), ^ soldierB5 = gcnew Soldier(Player::BLACK, 2);
+		Soldier^ soldierB4 = gcnew Soldier(Player::BLACK, 4), ^ soldierB5 = gcnew Soldier(Player::BLACK, 5);
 		General^ generalB = gcnew General(Player::BLACK, 0), ^ generalR = gcnew General(Player::RED, 0);
 		Chariot^ chariotR1 = gcnew Chariot(Player::RED, 1), ^ chariotR2 = gcnew Chariot(Player::RED, 2);
 		Horse^ horseR1 = gcnew Horse(Player::RED, 1), ^ horseR2 = gcnew Horse(Player::RED, 2);
@@ -27,7 +27,7 @@ namespace ProjectChineseChess
 		Advisor^ advisorR1 = gcnew Advisor(Player::RED, 1), ^ advisorR2 = gcnew Advisor(Player::RED, 2);
 		Cannon^ cannonR1 = gcnew Cannon(Player::RED, 1), ^ cannonR2 = gcnew Cannon(Player::RED, 2);
 		Soldier^ soldierR1 = gcnew Soldier(Player::RED, 1), ^ soldierR2 = gcnew Soldier(Player::RED, 2), ^ soldierR3 = gcnew Soldier(Player::RED, 3);
-		Soldier^ soldierR4 = gcnew Soldier(Player::RED, 1), ^ soldierR5 = gcnew Soldier(Player::RED, 2);
+		Soldier^ soldierR4 = gcnew Soldier(Player::RED, 4), ^ soldierR5 = gcnew Soldier(Player::RED, 5);
 		//將各種棋子的物件加進on_board
 		on_board["chariotB1"] = chariotB1; on_board["chariotB2"] = chariotB2; on_board["horseB1"] = horseB1; on_board["horseB2"] = horseB2;
 		on_board["elephantB1"] = elephantB1; on_board["elephantB2"] = elephantB2; on_board["advisorB1"] = advisorB1; on_board["advisorB2"] = advisorB2;
